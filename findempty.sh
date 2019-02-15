0e021aa5fdf7b114c6fdde654929e1d296977480
zeeshan6091 <zeeshanali6091@gmail.com>
2019-02-14 22:34:24 -0500

    first version

--- findempty.sh
+++ findempty.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+default_c="\e[0m"
+cyan_c="\e[36m"
+green_c="\e[92m"
+echo -e "${cyan_c}####### Empty File List #######${default_c}"
 
-echo "####### Empty File List ####### "
 ls $1|while read line
 do
 if [[ ! -s $line ]]
 then
-echo -e " \t$line "
+echo -e "${green_c} \t$line${default_c}"
+
 fi
 done
